34722eb1820d3a0ab0fcfee7122321721c270768
Aldrinsain <angelasain86@gmail.com>
2024-12-02 11:05:19 +0800

    adedded update task

--- page7.js
+++ page7.js
@@ -57,4 +57,12 @@ class TodoList {
         const buttons = taskItem.querySelectorAll('button');
         buttons.forEach(button => button.disabled = true);
     }
+    updateTask(taskText) {
+        
+        this.todoList.children[this.editingIndex].querySelector('.task-text').textContent = taskText;
+        
+       
+        this.resetEditing();
+    }
+
 
