e1113369636c37d9d7b7ba7daa94270e169bd87b
Aldrinsain <angelasain86@gmail.com>
2024-12-02 10:36:00 +0800

    added attendeance

--- page2.js
+++ page2.js
@@ -3,3 +3,25 @@ class LeafletMap {
     constructor(containerId, center, zoom) {
         this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
+        this.attendanceCountTEP = 0;
+        this.attendanceCountCSS = 0;
+        this.attendanceCountBA = 0;
+        this.attendanceCountGOAT= 0;
+        this.attendanceCountKiko= 0;
+
+        this.markerCounts = {};
+        this.markers = [];
+        this.loggedData = []; 
+
+        this.btn = document.getElementById('btn');
+        this.btn1 = document.getElementById('btn1');
+        this.btn2 = document.getElementById('btn2');
+        this.btn3 = document.getElementById('btn3');
+        this.btn4 = document.getElementById('btn4');
+        this.btnclear = document.getElementById('btnclear');
+        this.logCountElement = document.getElementById('logCountTEP');
+        this.logCount1Element = document.getElementById('logCountCCS');
+        this.logCount2Element = document.getElementById('logCountBA');
+        this.logCount3Element = document.getElementById('logCountGOAT');
+        this.logCount4Element = document.getElementById('logCountKkiko');
+        this.idContainer = document.getElementById('logContainer');
